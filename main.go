4b4190ae0e8bd75c428557deb81b0dfe43a3a811
Jaime Pillora <dev@jpillora.com>
2015-03-03 20:05:13 +1100

    prepare

--- main.go
+++ main.go
@@ -4,18 +4,16 @@ import (
 	"log"
 	"os"
 
-	// "github.com/jpillora/cloud-gox/server"
-
-	"./server/" //this warning is okay since we're 'go run'ning it
+	"github.com/jpillora/cloud-gox/server"
 )
 
 func main() {
-	//run server
+	//port
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
 	}
-
+	//run server
 	s := server.NewServer(port)
 	log.Printf("listening on %s...", port)
 	log.Fatal(s.Start())
